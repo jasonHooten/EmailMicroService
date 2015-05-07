8a2f0de8b9621e0d8ca32ccd18426b9bed348c13
Jason Hooten <Jasonh@qvinci.com>
2015-05-07 10:07:02 -0500

    mock email sending throuhg query string

--- controllers/email.js
+++ controllers/email.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+var emailService = require('../lib/semdEmail.js')
 var SendModel = require('../models/email/send');
 var ResendModel = require('../models/email/resend');
 
@@ -9,7 +9,15 @@ module.exports = function (router) {
 
     router.get('/send', function (req, res) {
         
+        //set timer
+        //authentic user
+                
         //send email logic here
+        emailService.sendEmail(req.query.to, req.query.from, req.query.subject, req.query.body);
+        
+        //log
+        //audit
+        //save to db
         
         var model = new SendModel();
         res.json(model);
@@ -17,9 +25,7 @@ module.exports = function (router) {
 
 
     router.get('/resend/:id', function (req, res) {
-        if(req.params.id === undefined) {
-            return res.status(500).json({ error: 'missing param: id'});
-        }
+        if(req.params.id === undefined) return res.status(500).json({ error: 'missing param: id'});
         var id = req.params.id;
         
         //send email logic here
